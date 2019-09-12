94bbb5f4d053f8156812b50c92d1a099b387510d
vittorduartte <mateusriograndense@gmail.com>
2019-09-11 23:15:45 -0300

    Implementação do arquivo main encarregado de rodar os primeiros testes da aplicação

---
+++ main.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+from DataClass.Disciplina import Disciplina
+from DataClass.Docente import Docente
+
+from Scrapper import Disciplina_Scraper, Docente_Scrapper
+import json
+
+def write_Data(data, name):
+    
+    file = open(name+'.json', 'w')
+
+    try:
+        file.write(data)
+        file.close()
+    except Exception as e:
+        print('Erro ao escrever os dados: ', e)
+
+
+disciplinas = Disciplina_Scraper.getDisciplinas()
+write_Data(json.dumps('{'+str(disciplinas)[1:-1]+'}'), 'disciplina_data')
+
+docentes = Docente_Scrapper.getDocentes(40)
+write_Data(docentes, 'docentes_data') 
+
+
+    
