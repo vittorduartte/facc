571ee2c33dbb68199352bf0dfa1e11fd2cf9d0fa
vittorduartte <mateusriograndense@gmail.com>
2019-09-11 23:14:07 -0300

    Implementação v0 da estrutura de dados e operações sobre os dados adquiridos.

---
+++ DataClass/Docente.py
@@ -0,0 +1,11 @@
+class Docente:
+
+    def __init__(self, siape, nome):
+    
+        self.siape = siape
+        self.nome = nome
+
+    def getDocente_JSON (self):
+
+        saida = "{'SIAPE':'"+self.siape+"', 'Nome':'"+self.nome+"'}"
+        return saida
